c028609ccd15e44b8c256ac2ae358f58787bb2f7
Douglas <douglaslima2363@gmail.com>
2020-06-22 19:26:13 -0300

    Cliente, TipoCliente, telefone e enderecos

--- src/main/java/com/udemy/cursomc/CursomcApplication.java
+++ src/main/java/com/udemy/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.udemy.cursomc.domain.Categoria;
 import com.udemy.cursomc.domain.Cidade;
+import com.udemy.cursomc.domain.Cliente;
+import com.udemy.cursomc.domain.Endereco;
 import com.udemy.cursomc.domain.Estado;
 import com.udemy.cursomc.domain.Produto;
+import com.udemy.cursomc.domain.enums.TipoCliente;
 import com.udemy.cursomc.repositories.CategoriaRepository;
 import com.udemy.cursomc.repositories.CidadeRepository;
+import com.udemy.cursomc.repositories.ClienteRepository;
+import com.udemy.cursomc.repositories.EnderecoRepository;
 import com.udemy.cursomc.repositories.EstadoRepository;
 import com.udemy.cursomc.repositories.ProdutoRepository;
 
@@ -24,12 +29,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -54,24 +65,33 @@ public class CursomcApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
-		
-		Estado est1 = new  Estado (null,"Minas Gerais");
-		Estado est2 = new Estado (null, "São Paulo");
-		
-		Cidade c1 = new Cidade (null, "Uberlândia",est1);
-		Cidade c2 = new Cidade (null, "São paulo",est2);
-		Cidade c3 = new Cidade (null, "Campinas", est2);
-		
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
 		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cl1 = new Cliente(null, "douglas", "douglas@gmail.com", "02856643108", TipoCliente.PESSOAFISICA);
+
+		cl1.getTelefones().addAll(Arrays.asList("35918411", "93862414"));
 		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-		
-		
+		clienteRepository.saveAll(Arrays.asList(cl1));
+
+		Endereco e1 = new Endereco(null, "Quadra 05 conjunto C", "23", "Condominio Morada dos nobres", "Sobradinho", "73030053", cl1, c1);
+		Endereco e2 = new Endereco(null, "QI 13 modulo L", "23", "Condominio Solar de Athenas", "sobradinho", "73030054", cl1, c2);
+
+		cl1.getEnderecos().addAll(Arrays.asList(e1, e2));
 
 		
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
 	}
 
--- src/main/java/com/udemy/cursomc/domain/Estado.java
+++ src/main/java/com/udemy/cursomc/domain/Estado.java
@@ -21,6 +21,7 @@ public class Estado implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 	@OneToMany (mappedBy = "estado")
