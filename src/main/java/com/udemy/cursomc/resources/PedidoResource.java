2f2bc2a6ccf6b5fef2ebbce321611f265ef8dc2c
douglas <voc@example.com>
2020-10-12 20:56:26 -0300

    Atualizando categoria com PUT

--- src/main/java/com/udemy/cursomc/resources/PedidoResource.java
+++ src/main/java/com/udemy/cursomc/resources/PedidoResource.java
@@ -18,7 +18,7 @@ public class PedidoResource {
 	private PedidoService pedido;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = pedido.buscar(id);
 
 		return ResponseEntity.ok().body(obj);
