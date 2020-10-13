bddcce8919609f0e3fc2da758d73e5a8b74aa185
douglas <voc@example.com>
2020-10-13 00:08:59 -0300

    Deletando uma categoria e, criando exception para deleção

--- src/main/java/com/udemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/udemy/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.udemy.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.cursomc.domain.Categoria;
@@ -25,11 +26,22 @@ public class CategoriaService {
 		obj.setId(null);// verificar se o ID esta NULL, caso esteja ele insere
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
-		
+
 	}
-	
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possível excluir uma categoria que possui produtos");
+		}
+
+	}
+
 }
